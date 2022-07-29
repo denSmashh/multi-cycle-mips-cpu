076986c1ecceb9b69447b8adf37c3a57e5dd0fb2
Den Smashnoy <s-noj02@yandex.ru>
2022-07-30 02:10:18 +0300

    add fdm in control unit

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -7,7 +7,7 @@ module control_unit (
     input logic [5:0]   funct,
     
     output logic        pc_write,
-    output logic        pc_src,
+    output logic [1:0]  pc_src,
     output logic        branch,
     output logic        mem_write,
     output logic        mem_to_reg,
@@ -31,25 +31,87 @@ logic [1:0] alu_op;
     MEM_ADR, 
     MEM_READ, 
     MEM_WRITEBACK, 
-    MEM_WRITE, EXEC, 
+    MEM_WRITE, 
+    EXEC, 
     ALU_WRITEBACK,
     BRANCH,
     ADDI_EXEC,
-    ADDI_WRITEBACK
+    ADDI_WRITEBACK,
+    JUMP
   } state_t;
 
 state_t state;
 state_t next_state;
  
+ // transition logic
  always @ (posedge clk) begin
-    if(~rstn)
-    else begin
-    
-    end
+    if(~rstn) state <= FETCH;
+    else state <= next_state;
  end
  
+ //next state logic
+ always_comb begin
+    case (state)
+        FETCH:          next_state = DECODE;
+        
+        DECODE: case(opcode)
+                    `LW_OP    : next_state = MEM_ADR;
+                    `SW_OP    : next_state = MEM_ADR;
+                    `R_TYPE_OP: next_state = EXEC;
+                    `BEQ_OP   : next_state = BRANCH;
+                    `ADDI_OP  : next_state = ADDI_EXEC;
+                    `JUMP_OP  : next_state = JUMP;
+                    default   : next_state = FETCH;     
+                endcase 
+        
+        MEM_ADR: case(opcode)
+                    `LW_OP:  next_state = MEM_READ;
+                    `SW_OP:  next_state = MEM_WRITE;
+                    default: next_state = FETCH;
+                 endcase
+        
+        MEM_READ:       next_state = MEM_WRITEBACK; 
+       
+        MEM_WRITEBACK:  next_state = FETCH; 
+        
+        MEM_WRITE:      next_state = FETCH;
+        
+        EXEC:           next_state = ALU_WRITEBACK;
+        
+        ALU_WRITEBACK:  next_state = FETCH;
+        
+        BRANCH:         next_state = FETCH;
+        
+        ADDI_EXEC:      next_state = ADDI_WRITEBACK;
+        
+        ADDI_WRITEBACK: next_state = FETCH;
+        
+        JUMP:           next_state = FETCH;
+     
+        default:        next_state = FETCH;
+        
+    endcase 
+ end
  
-    
+ //output logic
+ always_comb begin
+    case(state) 
+        FETCH:          begin IorD = 0; alu_src_A = 0; alu_src_B = 2'b01; alu_op = 2'b0; pc_src = 2'b00; end 
+        DECODE:         begin alu_src_A = 0; alu_src_B = 2'b11;  alu_op = 2'b0; end
+        MEM_ADR:        begin alu_src_A = 1; alu_src_B = 2'b10; alu_op = 2'b0; end
+        MEM_READ:       begin IorD = 1; end
+        MEM_WRITEBACK:  begin reg_dst = 0; mem_to_reg = 1; end
+        MEM_WRITE:      begin IorD = 1; end
+        EXEC:           begin alu_src_A = 1; alu_src_B = 2'b0; alu_op = 2'b10; end
+        ALU_WRITEBACK:  begin reg_dst = 1; mem_to_reg = 0; end
+        BRANCH:         begin alu_src_A = 1; alu_src_B = 2'b0; alu_op = 2'b01; pc_src = 2'b01; end
+        ADDI_EXEC:      begin alu_src_A = 1; alu_src_B = 2'b10; alu_op = 2'b0; end
+        ADDI_WRITEBACK: begin reg_dst = 0; mem_to_reg = 0; end
+        JUMP:           begin pc_src = 2'b10; end
+        default:        begin {pc_write, pc_src, branch, mem_write, mem_to_reg, reg_dst, reg_write, IorD, ir_write, alu_src_A, alu_src_B, alu_control} = 'b0; end
+    endcase
+ end
+ 
   
 //---------------------- ALU DECODER ---------------------------//
 always_comb begin
@@ -67,6 +129,5 @@ always_comb begin
     end
  endcase
 end
- 
-    
+  
 endmodule
