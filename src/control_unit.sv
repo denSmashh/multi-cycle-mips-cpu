0a3bae542ff9d47af446d303fd95c08d2360723a
Den Smashnoy <s-noj02@yandex.ru>
2022-07-30 01:03:26 +0300

    add alu decoder

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -10,6 +10,8 @@ module control_unit (
     output logic        pc_src,
     output logic        branch,
     output logic        mem_write,
+    output logic        mem_to_reg,
+    output logic        reg_dst,
     output logic        reg_write,
     output logic        IorD,
     output logic        ir_write,
@@ -18,7 +20,53 @@ module control_unit (
     output logic [2:0]  alu_control
     
 );
+
+logic [1:0] alu_op;    
     
+//--------------------- MAIN CONTROLLER (FSM) --------------------//
+ typedef enum logic [3:0] 
+ { 
+    FETCH, 
+    DECODE, 
+    MEM_ADR, 
+    MEM_READ, 
+    MEM_WRITEBACK, 
+    MEM_WRITE, EXEC, 
+    ALU_WRITEBACK,
+    BRANCH,
+    ADDI_EXEC,
+    ADDI_WRITEBACK
+  } state_t;
+
+state_t state;
+state_t next_state;
+ 
+ always @ (posedge clk) begin
+    if(~rstn)
+    else begin
     
+    end
+ end
+ 
+ 
+    
+  
+//---------------------- ALU DECODER ---------------------------//
+always_comb begin
+ case (alu_op)
+    2'b00: alu_control = `ALU_ADD;
+    2'b01: alu_control = `ALU_SUB;
+    default: begin 
+        case (funct)
+            `ADD_F: alu_control = `ALU_ADD;
+            `SUB_F: alu_control = `ALU_SUB;
+            `AND_F: alu_control = `ALU_AND;
+            `OR_F : alu_control = `ALU_OR;
+            `SLT_F: alu_control = `ALU_SLT;
+       endcase
+    end
+ endcase
+end
+ 
     
 endmodule
