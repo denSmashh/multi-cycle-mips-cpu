f5dee6db7fb50408015f252415cc5268bfac23c9
Den Smashnoy <s-noj02@yandex.ru>
2022-08-02 22:06:21 +0300

    add mips command for testing cpu

--- testbench/testbench_multicycle_mips.sv
+++ testbench/testbench_multicycle_mips.sv
@@ -2,46 +2,46 @@
 
 module testbench_multicycle_mips();
 
-cpu_top i_cpu_top 
-(
-
-);
- 
- 
- 
+logic clk,rstn;
+cpu_top i_cpu_top (.CLK(clk), .RSTn(rstn));
     
 // instruction and data memory reset (all memory 0x0)
-    integer i;
-    initial begin
-        //for(i = 0; i < 32; i = i + 1) 
-                //.instr_and_data_memory[i] = 0;
-    end
+integer i;
+initial begin
+    for(i = 0; i < ( i_cpu_top.i_ID_memory.INSTR_MEM_SIZE + i_cpu_top.i_ID_memory.DATA_MEM_SIZE); i = i + 1) 
+        i_cpu_top.i_ID_memory.instr_and_data_memory[i] = 0;
+end
     
-    // register file reset (all registers 0x0)
-    initial begin
-        //for(i = 0; i < 32; i = i + 1) 
-                //.mem_reg[i] = 0;
-    end
-    
-    
-    /*
+// register file reset (all registers 0x0)
+initial begin
+    for(i = 0; i < 32; i = i + 1) 
+        i_cpu_top.i_reg_file.mem_reg[i] = 0;
+end
+       
+  
     // load machine code
-    initial begin
-            i_cpu.i_instr_mem.mem[0] = 'h20030080;        // addi 0x0 0x3 0b0000000010000000    (write decimal 128 in register $3   )
-            i_cpu.i_instr_mem.mem[1] = 'h2004000F;        // addi 0x0 0x4 0b0000000000001111  (write decimal 16  in register $4   )
-            i_cpu.i_instr_mem.mem[2] = 'hAC040000;        // sw   0x0 0x4 0b0000000000000000    (store in mem[0x0+0x0] register $4  )   
-            i_cpu.i_instr_mem.mem[3] = 'h8C050000;        // lw   0x5 0x0 0b0000000000000000    (write in register $5 mem[0x0+0x0]  )
-            i_cpu.i_instr_mem.mem[4] = 'h10830010;        // beq  0x4 0x5 0x000c (if $4 == $5, branch in 12 steps; *true condition)
-            i_cpu.i_instr_mem.mem[5] = 'h00000000;        // nop (no operations)
-            i_cpu.i_instr_mem.mem[6] = 'h00000000;        // nop (no operations)
-            i_cpu.i_instr_mem.mem[7] = 'h10A4000C;        // beq (if $4 == $3, branch in 128 steps; *false condition)
-    end
-    */
+initial begin
+    i_cpu_top.i_ID_memory.instr_and_data_memory[0] = 'h20030080;    // addi 0x0 0x3 0b0000000010000000    (write decimal 128 in register $3   )
+    i_cpu_top.i_ID_memory.instr_and_data_memory[1] = 'h2004000F;    // addi 0x0 0x4 0b0000000000001111  (write decimal 16  in register $4   )
+    i_cpu_top.i_ID_memory.instr_and_data_memory[2] = 'hAC040010;    // sw   0x0 0x4 0b0000000000010000    (store in mem[0x00+0x10] register $4  )   
+    i_cpu_top.i_ID_memory.instr_and_data_memory[3] = 'h8C050010;    // lw   0x5 0x0 0b0000000000000000    (write in register $5 mem[0x0+0x10]  )
+    i_cpu_top.i_ID_memory.instr_and_data_memory[4] = 'h10A40003;    // beq  0x4 0x5 0x0003 (if $4 == $5, branch in 3 steps; *true condition)
+    i_cpu_top.i_ID_memory.instr_and_data_memory[5] = 'h00000000;    // nop (no operations)
+    i_cpu_top.i_ID_memory.instr_and_data_memory[6] = 'h10A40003;    //  (instruction #4)
+    i_cpu_top.i_ID_memory.instr_and_data_memory[7] = 'h10640010;    // beq (if $3 == $4, branch in 16 steps; *false condition)
+    i_cpu_top.i_ID_memory.instr_and_data_memory[8] = 'h00000000;    // nop (no operations)
+end
     
+    
+initial clk = 0;
+initial rstn = 1;
+    
+always #5 clk = ~clk;    
 
-    
+initial i_cpu_top.pc = 'b0;
     
 initial begin
+rstn = 0; #5; rstn = 1;
 
           
 end
